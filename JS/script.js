b089122fa925b864c6ed9fe49ce179532aee78d7
mbusseyjr <66703999+mbusseyjr@users.noreply.github.com>
2020-07-14 23:46:26 -0400

    HW V4 :JS toggle

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,30 @@
 $(".changeBtn1").click(function(){
-    console.log("click is working");
+    console.log("click is working Btn 1");
+
+    $(".firstDiv").addClass("firstDivNew");
+})
+
+$(".changeBtn2").click(function(){
+    console.log("click is working Btn 2");
+
+    $(".secondDiv").css("background","yellow");
+    $(".secondDiv").css("border-color", "blue");
+})
+
+$(".changeBtn3").click(function(){
+    console.log("click is working Btn 3");
+})
+
+$(".changeAllBtn").click(function(){
+    console.log("click is working Change All");
+
+    $(".firstDiv").addClass("changeAllColors");
+    $(".secondDiv").addClass("firstDivNew");
+    $(".thirdDiv").css("background", "orange");
+    $(".thirdDiv").css("border-color", "yellow");
+
+})
+
+$(".hideBtn1").click(function(){
+    $(".firstDiv").toggle();
 })
